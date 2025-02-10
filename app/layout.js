cc638aa1958e530e6d2980baea2a9bdc61c54dd5
satyaprakash singh <satyaprakash412770@gmail.com>
2025-02-10 17:49:55 +0530

    change title

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PAISA",
+  title: "Rupaya",
   description: "One stop Finance Platform",
 };
 
